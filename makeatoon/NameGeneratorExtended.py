b470f82a4829234c62edaea756570cc77db5109b
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-11 10:11:32 -0600

    Create NameGeneratorExtended.py

---
+++ makeatoon/NameGeneratorExtended.py
@@ -0,0 +1,89 @@
+from panda3d.core import DSearchPath
+
+from toontown.makeatoon.NameGenerator import NameGenerator
+
+
+class NameGeneratorExtended(NameGenerator):
+    def __init__(self, filename=None, sourcePath=None):
+        self.filename = filename
+        self.sourcePath = sourcePath
+        super().__init__()
+
+
+    def generateLists(self):
+        self.boyTitles = []
+        self.girlTitles = []
+        self.neutralTitles = []
+        self.boyFirsts = []
+        self.girlFirsts = []
+        self.neutralFirsts = []
+        self.capPrefixes = []
+        self.lastPrefixes = []
+        self.lastSuffixes = []
+        self.nameDictionary = {}
+        searchPath = DSearchPath()
+        if __debug__:
+            searchPath.appendDirectory(Filename('resources/phase_3/etc'))
+        if self.sp:
+            searchPath.appendDirectory(Filename(self.sp))
+
+        if self.filename:
+            filename = Filename(self.filename)
+        else:
+            filename = Filename(TTLocalizer.NameShopNameMaster)
+        found = vfs.resolveFilename(filename, searchPath)
+        if not found:
+            self.notify.error("NameGenerator: Error opening name list text file '%s.'" % filename)
+        input = StreamReader(vfs.openReadFile(filename, 1), 1)
+        currentLine = input.readline()
+        while currentLine:
+            if currentLine.lstrip()[0:1] != b'#':
+                a1 = currentLine.find(b'*')
+                a2 = currentLine.find(b'*', a1 + 1)
+                self.nameDictionary[int(currentLine[0:a1])] = (int(currentLine[a1 + 1:a2]), currentLine[a2 + 1:].rstrip().decode('utf-8'))
+            currentLine = input.readline()
+
+        masterList = [self.boyTitles,
+         self.girlTitles,
+         self.neutralTitles,
+         self.boyFirsts,
+         self.girlFirsts,
+         self.neutralFirsts,
+         self.capPrefixes,
+         self.lastPrefixes,
+         self.lastSuffixes]
+        for tu in list(self.nameDictionary.values()):
+            masterList[tu[0]].append(tu[1])
+
+        return 1
+
+
+    def totalNames(self):
+        totalNames = []
+        firsts = len(self.boyFirsts) + len(self.girlFirsts) + len(self.neutralFirsts)
+        totalNames.append('Total firsts: ' + str(firsts))
+        lasts = len(self.lastPrefixes) * len(self.lastSuffixes)
+        totalNames.append('Total lasts: ' + str(lasts))
+        neutralTitleFirsts = len(self.neutralTitles) * len(self.neutralFirsts)
+        boyTitleFirsts = len(self.boyTitles) * (len(self.neutralFirsts) + len(self.boyFirsts)) + len(self.neutralTitles) * len(self.boyFirsts)
+        girlTitleFirsts = len(self.girlTitles) * (len(self.neutralFirsts) + len(self.girlFirsts)) + len(self.neutralTitles) * len(self.girlFirsts)
+        totalTitleFirsts = neutralTitleFirsts + boyTitleFirsts + girlTitleFirsts
+        totalNames.append('Total title firsts: ' + str(totalTitleFirsts))
+        neutralTitleLasts = len(self.neutralTitles) * lasts
+        boyTitleLasts = len(self.boyTitles) * lasts
+        girlTitleLasts = len(self.girlTitles) * lasts
+        totalTitleLasts = neutralTitleLasts + boyTitleFirsts + girlTitleLasts
+        totalNames.append('Total title lasts: ' + str(totalTitleLasts))
+        neutralFirstLasts = len(self.neutralFirsts) * lasts
+        boyFirstLasts = len(self.boyFirsts) * lasts
+        girlFirstLasts = len(self.girlFirsts) * lasts
+        totalFirstLasts = neutralFirstLasts + boyFirstLasts + girlFirstLasts
+        totalNames.append('Total first lasts: ' + str(totalFirstLasts))
+        neutralTitleFirstLasts = neutralTitleFirsts * lasts
+        boyTitleFirstLasts = boyTitleFirsts * lasts
+        girlTitleFirstLasts = girlTitleFirsts * lasts
+        totalTitleFirstLasts = neutralTitleFirstLasts + boyTitleFirstLasts + girlTitleFirstLasts
+        totalNames.append('Total title first lasts: ' + str(totalTitleFirstLasts))
+        totalNames = firsts + lasts + totalTitleFirsts + totalTitleLasts + totalFirstLasts + totalTitleFirstLasts
+        totalNames.append('Total Names: ' + str(totalNames))
+        return totalNames
