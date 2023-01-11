387b4a0d8d4837155dc8e0d975afe62e63a0f736
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-11 10:11:22 -0600

    RenderClient: temp queue testing code

--- SnapshotRenderClient.py
+++ SnapshotRenderClient.py
@@ -1,4 +1,8 @@
 import asyncio
+import time
+
+from random import random
+
 import websockets
 import json
 
@@ -22,5 +26,71 @@ async def requestRender(renderParams: dict):
         await websocket.send(json.dumps(renderParams))
         response = await websocket.recv()
     return response
+
+import nest_asyncio
+
+nest_asyncio.apply()
+queueServ = asyncio.Queue()
+
+testDict = {
+    "TEST": f"{random()}"
+}
+# coroutine to generate work
+async def producer(queue, inputData):
+    print('Producer: Running')
+    # generate work
+    # for i in range(10):
+    # generate a value
+    value = random()
+    # block to simulate work
+    await asyncio.sleep(1)
+
+    # await asyncio.sleep(value)
+    # add to the queue
+    test = {
+        "TEST": f"{time.time()}"
+    }
+    await queue.put(inputData)
+    # send an all done signal
+    # await queue.put(None)
+    print('Producer: Done')
+
+
+# coroutine to consume work
+async def consumer(queue):
+    print('Consumer: Running')
+    # consume work
+    while True:
+        # get a unit of work
+        item = await queue.get()
+        print(item)
+
+        # await asyncio.sleep(5)
+
+        # check for stop signal
+        if item is None:
+            break
+
+        # result = json.loads(asyncio.get_event_loop().run_until_complete(requestRender(item)))
+        result = await requestRender(item)
+        # report
+        # result = json.loads(requestRender.response)
+        print(f'>got {result}')
+    # all done
+    print('Consumer: Done')
+
+
+# entry point coroutine
+async def main():
+    # create the shared queue
+    # run the producer and consumers
+    inputData = {
+        "TEST": f"{time.time()}"
+    }
+    await asyncio.gather(producer(queueServ, inputData), consumer(queueServ))
+
+
+# start the asyncio program
+# asyncio.run(main())
 
 # asyncio.get_event_loop().run_until_complete(test())
