e8bd0948bf9128013e2b9a9f1be3463aaeaf0604
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-11 10:05:54 -0600

    SnapshotBase: Code cleanup, support prefixes

--- snapshot/SnapshotBase.py
+++ snapshot/SnapshotBase.py
@@ -1,24 +1,33 @@
-# initial constants #
-# X and Y should be the same res (1:1), else there will be weird aspect ratio issues.
 from modtools.extensions.toon_snapshot.snapshot.RenderEnums import RenderType
-
-x = y = 1024
-headless = False
+from .. import SNAPSHOT_PREFIX
 
 if __name__ == "__main__":
-    from modular.toontown.toonbase import ModularStart
-    from modular.toontown.toonbase.ModularBase import ModularBase
+    from modtools.modbase import ModularStart
+    from modtools.modbase.ModularBase import ModularBase
+
     base = ModularBase()
     base.initNametagGlobals()
 
 from direct.directnotify import DirectNotifyGlobal
 from modtools.extensions.toon_snapshot.snapshot.OffscreenRenderBuffer import OffscreenRenderBuffer
 
+import typing
+
+if typing.TYPE_CHECKING:
+    try:
+        from otp.avatar.Avatar import Avatar
+        from otp.avatar.AvatarDNA import AvatarDNA
+        from toontown.makeatoon.NameGenerator import NameGenerator
+        from toontown.pets.PetNameGenerator import PetNameGenerator
+    except:
+        pass
+
 
 class SnapshotBase(OffscreenRenderBuffer):
     notify = DirectNotifyGlobal.directNotify.newCategory('SnapshotBase')
     notify.showTime = 1
 
+    # X and Y should be the same res (1:1), else there will be weird aspect ratio issues.
     def __init__(self, x=1024, y=1024, headless=False, filename="render.png"):
         """
         :param int x: Width resolution of buffer image.
@@ -29,25 +38,33 @@ class SnapshotBase(OffscreenRenderBuffer):
         self.buff = super().__init__(x, y)
         # Since we already initialized ShowBase and explicitly defined headless, we can set the value here.
         self.headless = headless
-        self.filename = filename
-        self.actor = None
-        self.actorDNA = None
-        self.nameGenerator = None
+        self.filename = SNAPSHOT_PREFIX + filename
+        self.actor = None  # type: Avatar
+        self.actorDNA = None  # type: AvatarDNA
+        self.nameGenerator = None  # type: NameGenerator | PetNameGenerator
         self.type = RenderType.Random
 
     def prepareActor(self, wantNametag):
         """
         Perform common operations for setting up a Render actor.
         """
+        # Parent our actor to our special render.
         self.actor.reparentTo(self.render)
+
+        # All actors are initially facing away from the camera. Let's fix this.
+        # self.actor.faceTowardsViewer()
         self.actor.setH(180)
+
+        # Drop shadows don't really look good in renders.
         self.actor.deleteDropShadow()
+
+        # Not really needed, but just in case we want to do any fancy calls:
         self.actor.doId = -1
 
+        # Should we display the nametag?
         if wantNametag:
             self.actor.nametag.manage(base.marginManager)
             self.actor.setNameVisible(1)
-
 
     def doSnapshot(self):
         """
@@ -63,7 +80,6 @@ class SnapshotBase(OffscreenRenderBuffer):
             self.render.reparentTo(render)
             self.cam.reparentTo(camera)
 
-
     def getInfo(self):
         return [
             self.actor.getName()
@@ -73,6 +89,8 @@ class SnapshotBase(OffscreenRenderBuffer):
         """
         Start over and clear the Actor information
         """
+        camera.setHpr(0, 0, 0)
+        camera.setPos(0, 0, 0)
         if hasattr(self.actor, 'delete'):
             self.actor.delete()
         if hasattr(self.actor, 'cleanup'):
