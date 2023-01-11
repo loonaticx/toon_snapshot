8978de86c0f4240b288bf4a53fa4f8ea07f9917b
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-11 10:08:12 -0600

    Discord Bot: code cleanup

--- SnapshotRenderDiscordBot.py
+++ SnapshotRenderDiscordBot.py
@@ -27,94 +27,89 @@ import nest_asyncio
 nest_asyncio.apply()
 
 guild = DISCORD_GUILD
-if guild == -1:
-    pass
-else:
+if guild.isdigit():
     MY_GUILD = discord.Object(id = DISCORD_GUILD)  # replace with your guild id
+else:
+    MY_GUILD = None
 
-RenderArgs = {
-    # /render
-    "RENDER_TYPE": RenderType.Toon,
-    "WANT_NAMETAG": True,
-    "CHAT_BUBBLE_TYPE": ChatBubbleType.Normal,
-    "CUSTOM_PHRASE": None,  # alt to speedchat phrase; not none = user gave input
-    "FRAME_TYPE": FrameType.Random,
-
-    # /render: context-specific
-    "NAME": None,  # generate random if None
-    "DNA_RANDOM": True,
-    "DNA_HAPHAZARD": False,  # Special cases like the Suits
-    "SPEEDCHAT_PHRASE": None,  # speedchat phrase id if not None
-    "POSE_PRESET": None,  # context specific, None -> random
-    "DNA_STRING": None,  # generate random dna if None, might be a literal dna (list; not netstring)
-
-    # /toon and /npc
-    "EYE_TYPE": EyeType.NormalOpen,
-    "MUZZLE_TYPE": MuzzleType.Neutral,
-
-    # /npc
-    "NPC_ID": None,  # none -> random
-
+# Since the NPC Toon dict is unfathomably large, let's pregenerate a lookup dict.
+NpcToonNames_name2id = {
+    toonName.lower().replace(" ", "").replace(".", ""): toonID for toonID, toonName in NPCToonNames.items()
 }
 
+
+# asyncio.run(prepare_bot_tree(DISCORD_GUILD != -1))
 
 class SnapshotRenderBot(commands.Bot):
     def __init__(self, intents, command_prefix, description):
         super().__init__(intents = intents, command_prefix = command_prefix, description = description)
+        self.pfp = None
 
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        if guild:
-            await self.tree.sync()
-        else:
+    async def prepare_bot_tree(self):
+        print(f"Preparing bot tree (guild = {MY_GUILD})")
+        # self.tree.clear_commands(guild = MY_GUILD)
+        self.tree.add_command(RenderGroup(bot), guild = MY_GUILD)
+
+        if MY_GUILD:
+            # This copies the global commands over to your guild.
             self.tree.copy_global_to(guild = MY_GUILD)
-            await self.tree.sync(guild = MY_GUILD)
+
+        await self.tree.sync(guild = MY_GUILD)
+
+    def setup_bot_pfp(self):
+        renderArgs = {
+            "RENDER_TYPE": RenderType.Toon,
+            "WANT_NAMETAG": False,
+            "FRAME_TYPE": FrameType.Headshot,
+
+        }
+        asyncio.get_event_loop().run_until_complete(SnapshotRenderClient.requestRender(renderArgs))
+        response = json.loads(SnapshotRenderClient.response)
+
+        pfp_path = response["RENDER_IMAGE"]
+        fp = open(pfp_path, 'rb')
+        self.pfp = fp.read()
+        return self.pfp
+
+    async def prepare_bot_presence(self):
+        # Since we're calling this from on_ready, which may be called multiple times, let's ensure we haven't
+        # setup a profile picture already.
+        if self.pfp:
+            return
+        # do an initial toon render here and set the bots pfp to it
+        # ( might be good to do a headshot/toptoons pic for it as well
+        self.setup_bot_pfp()
+        await self.user.edit(avatar = self.pfp)
+        await self.change_presence(activity = discord.Activity(type = discord.ActivityType.competing, name = "toontow"))
+
+    async def setup_hook(self):
+        """
+        A coroutine to be called to setup the bot, by default this is blank.
+        This performs an asynchronous setup after the bot is logged in,
+        but before it has connected to the Websocket (quoted from d.py docs)
+        """
+        await self.prepare_bot_tree()
+        print("Connecting...")
+        # if guild:
+        #     await self.tree.sync()
+        # else:
+        #     self.tree.copy_global_to(guild = MY_GUILD)
+        #     await self.tree.sync(guild = MY_GUILD)
+
+    async def on_ready(self):
+        """
+        Function called when the bot is ready. Emits the '[Bot] has connected' message
+        Loads the extensions
+        """
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+        await self.prepare_bot_presence()
 
 
 intents = discord.Intents.default()
 intents.message_content = True  # required for dropdown
 
 bot = SnapshotRenderBot(command_prefix = '/', description = 'blah', intents = intents)
-client = bot
-
-global bot_pfp
-bot_pfp = None
-
-
-def setup_bot_pfp():
-    global bot_pfp
-    if bot_pfp:
-        return
-    renderArgs = {
-        "RENDER_TYPE": RenderType.Toon,
-        "WANT_NAMETAG": False,
-        "FRAME_TYPE": FrameType.Headshot,
-
-    }
-    asyncio.get_event_loop().run_until_complete(SnapshotRenderClient.requestRender(renderArgs))
-    response = json.loads(SnapshotRenderClient.response)
-
-    pfp_path = response["RENDER_IMAGE"]
-    fp = open(pfp_path, 'rb')
-    bot_pfp = fp.read()
-    return bot_pfp
-
-
-setup_bot_pfp()
-
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
-    # do an initial toon render here and set the bots pfp to it
-    # ( might be good to do a headshot/toptoons pic for it as well
-    pfp = bot_pfp
-    await bot.user.edit(avatar = pfp)
-    await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.competing, name = "toontow"))
 
 
 def generateQuestEmbed(avName="Toon", questID=None):
@@ -137,80 +132,6 @@ def generateQuestEmbed(avName="Toon", questID=None):
     return (em, file)
 
 
-def generateNpcInfo(npcID):
-    # set NpcInfo attrs here by looking up npcID stuff
-    print(npcID)
-    print(NPCToons.NPCToonDict.get(npcID)[0])
-    bldg = localizer.zone2TitleDict[NPCToons.NPCToonDict.get(npcID)[0]]
-    info = {
-        # 2309(npcid)[0] = bldgid
-        "Building": bldg,
-        "Location": "Punchline Place, Toontown Central",
-        "Type": "Shopkeeper",
-        "Random DNA": "False"
-    }
-    return info
-
-
-def generateNPCRender(renderArgs):
-    """
-    DO NOT DIRECTLY CALL FROM ME unless you are part of the toon command
-    """
-    # generate toonsnapshot, pass args if needed?
-    asyncio.get_event_loop().run_until_complete(SnapshotRenderClient.requestRender(renderArgs))
-
-    with open(SnapshotRenderClient.response, 'rb') as f:
-        fname = SnapshotRenderClient.response.replace(f"{SNAPSHOT_DIR}/", "")
-        file = discord.File(SnapshotRenderClient.response)
-
-    # file = discord.File(f"{OP_DIR}/test/image_test.png", filename = "image_test.png")
-
-    # get npc info
-    npcID = renderArgs.get("NPC_ID")
-    if not npcID:
-        npcID = random.choice(list(NPCToons.NPCToonDict.items()))[0]
-    npcInfo = generateNpcInfo(npcID)
-    # RenderArgs["RENDER_TYPE"] = RenderType.NPC
-
-    em = discord.Embed(
-        title = "NPC Toon",
-        description = f"{NPCToonNames.get(npcID)}",
-    )
-    em.set_image(url = f"attachment://{fname}")
-
-    for name, value in npcInfo.items():
-        em.add_field(name = name, value = value)
-
-    em.set_footer(text = f"NPC ID: {npcID} (420 NPCs total)")
-    return em, file
-
-
-# Defines a custom Select containing colour options
-# that the user can choose. The callback function
-# of this class is called when the user changes their choice
-class Dropdown(discord.ui.Select):
-    def __init__(self):
-        # Set the options that will be presented inside the dropdown
-        options = [
-            discord.SelectOption(label = 'Red', description = 'Your favourite colour is red', emoji = '🟥'),
-            discord.SelectOption(label = 'Green', description = 'Your favourite colour is green', emoji = '🟩'),
-            discord.SelectOption(label = 'Blue', description = 'Your favourite colour is blue', emoji = '🟦'),
-        ]
-
-        # The placeholder is what will be shown when no option is chosen
-        # The min and max values indicate we can only pick one of the three options
-        # The options parameter defines the dropdown options. We defined this above
-        super().__init__(placeholder = 'Choose your favourite colour...', min_values = 1, max_values = 1,
-                         options = options)
-
-    async def callback(self, interaction: discord.Interaction):
-        # Use the interaction object to send a response message containing
-        # the user's favourite colour or choice. The self object refers to the
-        # Select object, and the values attribute gets a list of the user's
-        # selected options. We only want the first one.
-        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
-
-
 # Define a simple View that gives us a confirmation menu
 class ToggleRenderAttributesView(discord.ui.View):
     def __init__(self, embed):
@@ -220,7 +141,6 @@ class ToggleRenderAttributesView(discord.ui.View):
 
     # When the confirm button is pressed, set the inner value to `True` and
     # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
     @discord.ui.button(label = 'Toggle Nametag', style = discord.ButtonStyle.secondary)
     async def toggle_nametag(self, interaction: discord.Interaction, button: discord.ui.Button):
         file = discord.File(f"{OP_DIR}/img/test/image_test_full.png", filename = "image_test_full.png")
@@ -242,53 +162,12 @@ class GenerateQuestView(discord.ui.View):
         super().__init__()
         self.icon = icon
 
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
     @discord.ui.button(label = 'Random ToonTask', style = discord.ButtonStyle.primary)
     async def regenerate_quest(self, interaction: discord.Interaction, button: discord.ui.Button):
         em, file = generateQuestEmbed(interaction.user.display_name)
         await interaction.response.edit_message(embed = em, view = self)
 
 
-class DropdownView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-
-        # Adds the dropdown to our view object.
-        self.add_item(Dropdown())
-
-
-@bot.command()
-async def color(ctx):
-    """Sends a message with our dropdown containing colours"""
-
-    # Create the view containing our dropdown
-    view = DropdownView()
-
-    # Sending a message containing our view
-    await ctx.send('Pick your favourite colour:', view = view)
-
-
-@bot.tree.command()
-async def hello(interaction: discord.Interaction):
-    """Says hello!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
-
-
-@bot.tree.command()
-@app_commands.describe(
-    first_value = 'The first value you want to add something to',
-    second_value = 'The value you want to add to the first value',
-)
-async def add(interaction: discord.Interaction, first_value: int, second_value: int):
-    """Adds two numbers together."""
-    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
-
-
-# The rename decorator allows us to change the display of the parameter on Discord.
-# In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
-# Note that other decorators will still refer to it as `text_to_send` in the code.
 @bot.tree.command()
 async def quest(interaction: discord.Interaction, quest_id: Optional[int] = None):
     # QuestDialogDict may only be mainline tasks though.? if QuestDict focuses on JFF this will add on to the count
@@ -373,32 +252,6 @@ async def knockknock(interaction: discord.Interaction, general_jokes: Optional[b
 
 
 @bot.tree.command()
-async def emtest(interaction: discord.Interaction):
-
-    # file2 = discord.File(f"{OP_DIR}/test/image_toon_full.png", filename = "image_toon_full.png")
-
-    # embed = discord.Embed()
-    # await channel.send(file = file, embed = embed)
-    em, file = generateNPCRender(RenderArgs)
-
-    # in future, pass file since it'll be re=rendered
-    # NB: make it easier just re-render and use that output??? but then cache.. fuck
-    # maybe have an option to pass a custom file name to toonrender
-    view = ToggleRenderAttributesView(embed = em)
-
-    await interaction.response.send_message(files = [file], embed = em, view = view)
-    await view.wait()
-    if view.value is None:
-        print('Timed out...')
-    elif view.value:
-        # this is where you would re-call the toon render operation?
-        # actually this might be called after the view :b so no
-        print("confirm")
-    else:
-        print('Cancelled...')
-
-
-@bot.tree.command()
 async def toontip(interaction: discord.Interaction,
                   general_tip: Optional[bool] = True, street_tip: Optional[bool] = True,
                   minigame_tip: Optional[bool] = True, coghq_tip: Optional[bool] = True,
@@ -427,16 +280,16 @@ async def toontip(interaction: discord.Interaction,
         await interaction.response.send_message(f"Hey! You need at least one option to be True!", ephemeral = True)
         return
 
+    tip = random.choice(tipChoices)
+
     """Sends the text into the current channel."""
-    if count:
-        # QuestDialogDict may only be mainline tasks though.? if QuestDict focuses on JFF this will add on to the count
-        await interaction.response.send_message(f"Total Toon Tip Entries = {len(tipChoices)}")
-    else:
-        tip = random.choice(tipChoices)
-        await interaction.response.send_message(tip)
+    em = discord.Embed(
+        title = "Toon Tip",
+        description = tip
+    )
+    em.set_footer(text = f"Total Toon Tips loaded: {len(tipChoices)})")
 
-
-# todo: option to preserve renders in out/
+    await interaction.response.send_message(embed = em)
 
 
 # @app_commands.guild_only()
@@ -445,7 +298,6 @@ class RenderGroup(app_commands.Group):
         super().__init__()
         self.bot = bot
         self.name = "render"
-
 
     def render_image(self, render_config):
         # run the render process
@@ -483,7 +335,7 @@ class RenderGroup(app_commands.Group):
     @app_commands.describe(
         random_dna = 'Render a Random Toon',
         npc = "Name of NPC to render",
-        toon_name = "Name of toon",
+        toon_name = "Name of Toon",
         nametag = "Display nametag?"
     )
     @app_commands.autocomplete(
@@ -496,7 +348,8 @@ class RenderGroup(app_commands.Group):
             frame_type: Optional[FrameType] = FrameType.Bodyshot,
             eye_type: Optional[EyeType] = EyeType.NormalOpen,
             chatbubble_type: Optional[ChatBubbleType] = ChatBubbleType.Normal,
-            say: Optional[str] = None):
+            say: Optional[str] = None, muzzle_type: MuzzleType = None
+    ):
         renderConfig = RenderSettings.RenderSettings().renderConfig
 
         # ToonSnapshot will handle it from here ~
@@ -506,13 +359,27 @@ class RenderGroup(app_commands.Group):
         renderConfig["CUSTOM_PHRASE"] = say
         renderConfig["CHAT_BUBBLE_TYPE"] = chatbubble_type
         renderConfig["EYE_TYPE"] = eye_type
+        renderConfig["MUZZLE_TYPE"] = muzzle_type
+
         if not npc:
             renderConfig["RENDER_TYPE"] = RenderType.Toon
         else:
             renderConfig["RENDER_TYPE"] = RenderType.NPC
-            # todo: check to see if user passed actual npc toon name manually,
-            # convert the name to the id if possible
-            renderConfig["NPC_ID"] = int(npc.replace('npc-', ''))
+            # see if the user chose an autocomplete or manually typed in their selection
+            # autocomplete will give us the npcid with an npc- prefix
+            npcID = npc.replace('npc-', '')
+            if npcID.isdigit():
+                renderConfig["NPC_ID"] = int(npcID)
+            else:
+                # User tried passing in the name of an npc, most likely
+                # get rid of spaces and period requirements
+                npcName = npcID.replace(" ", "").replace(".", "")
+                npcID = NpcToonNames_name2id.get(npcName)
+                if not npcID:
+                    await interaction.response.send_message("Sorry, I couldn't find that NPC Toon!", ephemeral = True)
+                    return
+                else:
+                    renderConfig["NPC_ID"] = npcID
             # em, file = generateNPCRender(RenderArgs)
         file, embed = self.render_image(renderConfig)
 
@@ -524,33 +391,30 @@ class RenderGroup(app_commands.Group):
         await interaction.response.send_message(file = file, embed = embed)
 
     @app_commands.command(name = "doodle")
+    @app_commands.describe(
+        random_dna = 'unfinished',
+        say = "Make Doodle say input phrase",
+        doodle_name = "Name of Doodle (default random)",
+        nametag = "Display nametag? (defualt True)"
+    )
     async def render_doodle(
-            self, interaction: discord.Interaction, random_dna: Optional[bool] = True,
+            self, interaction: discord.Interaction,
+            random_dna: Optional[bool] = True,
             say: Optional[str] = None, doodle_name: Optional[str] = None, nametag: Optional[bool] = True,
+            chatbubble_type: Optional[ChatBubbleType] = ChatBubbleType.Normal,
 
-    ) -> None:
+    ):
         renderConfig = RenderSettings.RenderSettings().renderConfig
 
         renderConfig["RENDER_TYPE"] = RenderType.Doodle
-        renderConfig["DNA_RANDOM"] = random_dna
+        # renderConfig["DNA_RANDOM"] = random_dna
         renderConfig["NAME"] = doodle_name
         renderConfig["WANT_NAMETAG"] = nametag
         renderConfig["CUSTOM_PHRASE"] = say
+        renderConfig["CHAT_BUBBLE_TYPE"] = chatbubble_type
 
         file, embed = self.render_image(renderConfig)
         await interaction.response.send_message(file = file, embed = embed)
-
-    def generateNpcInfo(self, npcID):
-        # set NpcInfo attrs here by looking up npcID stuff
-        bldg = localizer.zone2TitleDict[NPCToons.NPCToonDict.get(npcID)[0]][0]
-        info = {
-            # 2309(npcid)[0] = bldgid
-            "Building": bldg,
-            "Location": "Punchline Place, Toontown Central (placeholder)",
-            "Type": "Shopkeeper (placeholder)",
-            "Random DNA": "False (placeholder)"
-        }
-        return info
 
     @app_commands.command(name = "suit")
     async def render_suit(
@@ -574,11 +438,17 @@ class RenderGroup(app_commands.Group):
         file, embed = self.render_image(renderConfig)
         await interaction.response.send_message(file = file, embed = embed)
 
+    def generateNpcInfo(self, npcID):
+        # set NpcInfo attrs here by looking up npcID stuff
+        bldg = localizer.zone2TitleDict[NPCToons.NPCToonDict.get(npcID)[0]][0]
+        info = {
+            # 2309(npcid)[0] = bldgid
+            "Building": bldg,
+            "Location": "Punchline Place, Toontown Central (placeholder)",
+            "Type": "Shopkeeper (placeholder)",
+            "Random DNA": "False (placeholder)"
+        }
+        return info
 
-
-if guild == -1:
-    bot.tree.add_command(RenderGroup(bot))
-else:
-    bot.tree.add_command(RenderGroup(bot), guild = discord.Object(id = DISCORD_GUILD))
 
 bot.run(DISCORD_TOKEN)
