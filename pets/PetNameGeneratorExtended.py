2b21ad401af4f2022e3f6014e7ffdd81313b65bc
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-28 15:23:55 -0600

    PetNameGenerator: pretty formatting

--- pets/PetNameGeneratorExtended.py
+++ pets/PetNameGeneratorExtended.py
@@ -10,10 +10,10 @@ class PetNameGeneratorExtended(PetNameGenerator.PetNameGenerator):
         self.generateLists()
 
     def generateLists(self):
-        """ This method looks in a text file specified in the localizer and loads
-            in all the names into the 8 lists as well as populating self.nameDictionary
-            which has uniqueIDs mapped to a tuple of category and name
-            """
+        """
+        Looks in a text file specified in the localizer and loads in all the names into the 8 lists
+        as well as populating self.nameDictionary which has uniqueIDs mapped to a tuple of category and name
+        """
         self.boyFirsts = []
         self.girlFirsts = []
         self.neutralFirsts = []
@@ -46,13 +46,19 @@ class PetNameGeneratorExtended(PetNameGenerator.PetNameGenerator):
             masterList[tu[0]].append(tu[1])
         return 1
 
-    def randomName(self, gender = None, seed = None):
+    def randomName(self, gender=None, seed=None):
         S = random.getstate()
+
         if seed is not None:
             random.seed(seed)
+
         if gender is None:
+            # Randomly pick the name sex
             gender = random.choice([0, 1])
+
         retString = ''
+
+        # Shallow copy, since we will be altering the list
         firstList = self.neutralFirsts[:]
         if gender == 0:
             firstList += self.boyFirsts
@@ -60,6 +66,9 @@ class PetNameGeneratorExtended(PetNameGenerator.PetNameGenerator):
             firstList += self.girlFirsts
         else:
             self.error('Must be boy or girl.')
+
         retString += random.choice(firstList).decode('utf-8')
+
         random.setstate(S)
+
         return retString
